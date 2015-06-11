32f848a45db5697f2cad681ba604b8c3f4d365d1
zhujohnle <545948101@qq.com>
2015-06-11 10:52:11 +0800

    删除无用工程

--- ConvPlatformApp/src/com/zy/booking/control/OnEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.zy.booking.control;
-
-public interface OnEventListener {
-	public void onClickListner(int id);
-	public void onLongClickListener(int id);
-
-}
--- ConvPlatformApp/src/com/zy/booking/view/HorizontalGallery.java
+++
@@ -1,5 +0,0 @@
-package com.zy.booking.view;
-
-public class HorizontalGallery {
-
-}
