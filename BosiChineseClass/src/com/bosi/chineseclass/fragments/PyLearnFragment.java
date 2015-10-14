f0deb1d2d48336d33a3975aee8ddf36b81eafc52
zhujohnle <545948101@qq.com>
2015-10-14 19:25:12 +0800

    修改因为可能是屏幕分辨率的问题导致的 不能显示ing

--- BosiChineseClass/src/com/bosi/chineseclass/fragments/PyLearnFragment.java
+++ BosiChineseClass/src/com/bosi/chineseclass/fragments/PyLearnFragment.java
@@ -130,7 +130,7 @@ public class PyLearnFragment extends BaseFragment implements DownLoadInterface ,
 	private int getTextSize(){
 		int width = Utils.getWinWidth(mActivity);
 		if(width>900)return 19;
-		return 15;
+		return 14;
 	}
 	
 	private void setup() {
